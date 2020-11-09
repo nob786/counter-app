cbc5efb5a4d9a4d05d1891af9f71065f9b6cdb07
nob786 <44703244+nob786@users.noreply.github.com>
2020-11-09 14:01:52 +0500

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  /* constructor() {
+    super();
+    console.log("This prop", this);
+  }*/
+
+  render() {
+    // console.log("props", this.props);
+
+    return (
+      <div className = "row"> 
+
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        </div>
+
+
+        <div className="col">
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-primary btn-sm m-2"
+        >
+          +
+        </button>
+
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-primary btn-sm"
+          disabled = {this.props.counter.value === 0 ? "disabled" : "" }
+        >
+          -
+        </button>
+
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-warning btn-sm m-2"
+        >
+          Delete
+        </button>
+
+        </div>
+       
+        
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
